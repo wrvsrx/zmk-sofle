9660a18a3aa0c768bd647aaf4d2e9da142a5f93b
wrvsrx <wrvsrx@outlook.com>
2024-11-22 15:45:59 +0800

    chore(build): add build system

--- default.nix
+++ default.nix
@@ -1,9 +1,26 @@
-{ stdenvNoCC }:
-stdenvNoCC.mkDerivation {
-  name = "";
-  src = ./.;
-  nativeBuildInputs = [ ];
-  installPhase = ''
-    mkdir -p $out
-  '';
+{ buildKeyboard }:
+{
+  packages = {
+    sofle_reset = buildKeyboard {
+      name = "sofle_reset";
+      src = ./.;
+      board = "nice_nano_v2";
+      shield = "settings_reset";
+      zephyrDepsHash = "sha256-H/ZlKyRiEZmDkVE3SSVcFsEF9tQ7XOCgbQ2tPpO2Pys=";
+    };
+    sofle_left = buildKeyboard {
+      name = "sofle_left";
+      src = ./.;
+      board = "sofle_left";
+      shield = "nice_view_adapter nice_view";
+      zephyrDepsHash = "sha256-H/ZlKyRiEZmDkVE3SSVcFsEF9tQ7XOCgbQ2tPpO2Pys=";
+    };
+    sofle_right = buildKeyboard {
+      name = "sofle_right";
+      src = ./.;
+      board = "sofle_right";
+      shield = "nice_view_adapter nice_view";
+      zephyrDepsHash = "sha256-H/ZlKyRiEZmDkVE3SSVcFsEF9tQ7XOCgbQ2tPpO2Pys=";
+    };
+  };
 }
