707b5d5772f7c4826e801b0c152eb7b30734d405
wrvsrx <wrvsrx@outlook.com>
2024-11-25 00:52:34 +0800

    chore(env): update something

--- flake.nix
+++ flake.nix
@@ -8,7 +8,10 @@
     west2nix = {
       url = "github:adisbladis/west2nix";
       inputs.nixpkgs.follows = "nixpkgs";
+      inputs.zephyr-nix.follows = "zephyr-nix";
     };
+    zephyr-nix.url = "github:adisbladis/zephyr-nix";
+    zephyr-nix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs =
@@ -20,11 +23,13 @@
         perSystem =
           { pkgs, system, ... }:
           let
-            s = pkgs.callPackage ./. { };
+            s = pkgs.callPackage ./. {
+              west2nix = inputs.west2nix.lib.mkWest2nix { inherit pkgs; };
+              zephyr = inputs.zephyr-nix.packages.${system};
+            };
           in
           rec {
-
-            # inherit (s) packages;
+            inherit (s) packages;
             devShells.default = pkgs.mkShell {
               # inputsFrom = [ packages.default ];
               nativeBuildInputs = [ inputs.west2nix.packages.${system}.default ];
