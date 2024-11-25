c2a25843b57a969eb7e40a96965b8314e04c9a48
wrvsrx <wrvsrx@outlook.com>
2024-11-25 15:32:27 +0800

    chore(build): refactor nix code

--- default.nix
+++ default.nix
@@ -14,105 +14,60 @@ let
   west2nixHook = west2nix.mkWest2nixHook {
     manifest = ./west2nix.toml;
   };
-
+  buildSofle =
+    { board, shields }:
+    stdenv.mkDerivation {
+      name = board;
+      src = ./.;
+      buildInputs = [
+        (zephyr.sdk.override {
+          targets = [
+            "arm-zephyr-eabi"
+          ];
+        })
+        west2nixHook
+        zephyr.pythonEnv
+        zephyr.hosttools-nix
+        gitMinimal
+        cmake
+        ninja
+      ];
+      dontUseCmakeConfigure = true;
+      env.Zephyr_DIR = "../zephyr/share/zephyr-package/cmake";
+      westBuildFlags = [
+        "-s"
+        "../zmk/app"
+        "-b"
+        board
+        "--"
+        "-DZMK_CONFIG=${./config}"
+        "-DSHIELD=${lib.concatStringsSep ";" shields}"
+      ];
+      installPhase = ''
+        mkdir $out
+        cp ./build/zephyr/zmk.uf2 $out/$name.uf2
+      '';
+    };
 in
 {
   packages = {
-    sofle_reset = stdenv.mkDerivation {
-      name = "sofle_reset";
-      src = ./.;
-      buildInputs = [
-        (zephyr.sdk.override {
-          targets = [
-            "arm-zephyr-eabi"
-          ];
-        })
-        west2nixHook
-        zephyr.pythonEnv
-        zephyr.hosttools-nix
-        gitMinimal
-        cmake
-        ninja
-      ];
-      dontUseCmakeConfigure = true;
-      env.Zephyr_DIR = "../zephyr/share/zephyr-package/cmake";
-      westBuildFlags = [
-        "-s"
-        "../zmk/app"
-        "-b"
-        "nice_nano_v2"
-        "--"
-        "-DZMK_CONFIG=${./config}"
-        "-DSHIELD=settings_left"
-      ];
-      installPhase = ''
-        mkdir $out
-        cp ./build/zephyr/zmk.elf $out/
-      '';
+    sofle_reset = buildSofle {
+      board = "nice_nano_v2";
+      shields = [ "settings_left" ];
     };
-    sofle_left = stdenv.mkDerivation {
-      name = "sofle_left";
-      src = ./.;
-      buildInputs = [
-        (zephyr.sdk.override {
-          targets = [
-            "arm-zephyr-eabi"
-          ];
-        })
-        west2nixHook
-        zephyr.pythonEnv
-        zephyr.hosttools-nix
-        gitMinimal
-        cmake
-        ninja
+    sofle_left = buildSofle {
+      board = "sofle_left";
+      shields = [
+        "nice_view_adapter"
+        "nice_view"
       ];
-      dontUseCmakeConfigure = true;
-      env.Zephyr_DIR = "../zephyr/share/zephyr-package/cmake";
-      westBuildFlags = [
-        "-s"
-        "../zmk/app"
-        "-b"
-        "sofle_left"
-        "--"
-        "-DZMK_CONFIG=${./config}"
-        "-DSHIELD=nice_view_adapter;nice_view"
-      ];
-      installPhase = ''
-        mkdir $out
-        cp ./build/zephyr/zmk.elf $out/
-      '';
     };
-    sofle_right = stdenv.mkDerivation {
-      name = "sofle_right";
-      src = ./.;
-      buildInputs = [
-        (zephyr.sdk.override {
-          targets = [
-            "arm-zephyr-eabi"
-          ];
-        })
-        west2nixHook
-        zephyr.pythonEnv
-        zephyr.hosttools-nix
-        gitMinimal
-        cmake
-        ninja
+    sofle_right = buildSofle {
+      board = "sofle_right";
+      shields = [
+        "nice_view_adapter"
+        "nice_view"
       ];
-      dontUseCmakeConfigure = true;
-      env.Zephyr_DIR = "../zephyr/share/zephyr-package/cmake";
-      westBuildFlags = [
-        "-s"
-        "../zmk/app"
-        "-b"
-        "sofle_right"
-        "--"
-        "-DZMK_CONFIG=${./config}"
-        "-DSHIELD=nice_view_adapter;nice_view"
-      ];
-      installPhase = ''
-        mkdir $out
-        cp ./build/zephyr/zmk.elf $out/
-      '';
     };
   };
 }
