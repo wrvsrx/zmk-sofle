73f624e351ab9ba81c238aedb18ec9f1876b4636
wrvsrx <wrvsrx@outlook.com>
2024-12-04 22:43:35 +0800

    chore: use pythonPackages from nur

--- nix/default.nix
+++
@@ -1,5 +0,0 @@
-{ pkgs }:
-rec {
-  keymap-drawer = pkgs.python3.pkgs.callPackage ./keymap-drawer { inherit tree-sitter-devicetree; };
-  tree-sitter-devicetree = pkgs.python3.pkgs.callPackage ./tree-sitter-devicetree { };
-}
